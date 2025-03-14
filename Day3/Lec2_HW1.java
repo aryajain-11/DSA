ab0e82c3eb9f9e6888ed1264af04f22466bc25e9
aryajain-11 <jain2004arya@gmail.com>
2025-03-14 15:18:22 +0530

    Initial Commmit

---
+++ Day3/Lec2_HW1.java
@@ -0,0 +1,64 @@
+public class Lec2_HW1 {
+    public static void main(String[] args) {
+        //hollow butterfly
+
+        int n =5;
+        //upper half
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=5; j++) {
+                if (j ==1|| i==j) {
+                    System.out.print("*");
+                    
+                }
+                else{
+                    System.out.print(" ");
+                }
+               
+            }
+            for (int j = 1; j <=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                if (j ==n|| i==j) {
+                    System.out.print("*");
+                    
+                }
+                else{
+                    System.out.print(" ");
+                }
+               
+            }
+
+            System.out.println();
+        }
+
+        //lower half
+        for (int i = n; i >=1; i--) {
+            for (int j = 1; j <=5; j++) {
+                if (j ==1|| i==j) {
+                    System.out.print("*");
+                    
+                }
+                else{
+                    System.out.print(" ");
+                }
+               
+            }
+            for (int j = 1; j <=2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                if (j ==1|| i==j) {
+                    System.out.print("*");
+                    
+                }
+                else{
+                    System.out.print(" ");
+                }
+               
+            }
+
+            System.out.println();
+        }
+    }
+}
