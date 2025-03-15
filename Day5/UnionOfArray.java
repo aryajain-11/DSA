5c4dedb2d12d0b55e4372e227201a88e5d5f6d5c
aryajain-11 <jain2004arya@gmail.com>
2025-03-15 15:23:12 +0530

    initial commit

---
+++ Day5/UnionOfArray.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionOfArray {
+    public static void main(String[] args) {
+        UnionOfArray a = new UnionOfArray();
+        int arr1[] = {2, 2, 3, 4, 4, 4, 7, 8};
+        int arr2[] = {1, 2, 2, 2, 4, 6, 6, 6, 7, 8, 10};
+        List<Integer> finalUnion = new ArrayList<>();
+    
+        a.unionOfTwoSortedArray(arr1, arr2, finalUnion);
+        System.out.println(finalUnion);
+
+        
+    }
+    public void unionOfTwoSortedArray(int[] arr1, int[] arr2, List<Integer> finalList) {
+        int i = 0;
+        int j = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            while ((i < arr1.length - 1) && (arr1[i] == arr1[i + 1])) {
+              i++;
+            }
+      
+            while ((j < arr2.length - 1) && (arr2[j] == arr2[j + 1])) {
+              j++;
+            }
+      
+            if (arr1[i] < arr2[j]) {
+              finalList.add(arr1[i++]);
+            } else if (arr1[i] > arr2[j]) {
+              finalList.add(arr2[j++]);
+            } else {
+              finalList.add(arr1[i]);
+              i++;
+              j++;
+            }
+          }
+      
+          while (i < arr1.length) {
+            if (i < arr1.length - 1 && arr1[i] == arr1[i + 1]) {
+              i++;
+            } else {
+              finalList.add(arr1[i++]);
+            }
+          }
+      
+          while (j < arr2.length) {
+            if (j < arr2.length - 1 && arr2[j] == arr2[j + 1]) {
+              j++;
+            } else {
+              finalList.add(arr2[j++]);
+            }
+          }
+       }
+}  
+    
+
+
+
+    
