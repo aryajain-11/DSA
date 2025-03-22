3c28c0aecb64794623b4c3cf34c357e63e177cd5
aryajain-11 <jain2004arya@gmail.com>
2025-03-22 11:47:05 +0530

    Initial Commit

--- Day9/SelectionSort.java
+++ Day9/SelectionSort.java
@@ -1,26 +1,26 @@
 public class SelectionSort {
-    public static void main(String[] args) {
-        int arr[] = {7,8,3,2};
-        //selection sort
+    public static void selectionSort(int arr[]){
         for (int i = 0; i < arr.length-1; i++) {
-            int smallest = i;
+            int minindex = i;
             for (int j = i+1; j < arr.length; j++) {
-                if (arr[smallest]>arr[j]) {
-                    smallest = j;
-                    
+                if (arr[minindex]>arr[j]) {
+                    minindex= j;
                 }
+
             }
-            int temp = arr[smallest];
-            arr[smallest]= arr[i];
-            arr[i] = temp;
+            int temp = arr[minindex];
+            arr[minindex]= arr[i];
+            arr[i]= temp;
         }
-        printArray(arr);
     }
-    public static void printArray(int arr[]){
+    public static void main(String[] args) {
+        int arr[] = {7,5,4,1,3};
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+ " ");
+            selectionSort(arr);
+            System.out.println(arr[i]);
         }
-        System.out.println();
+
     }
 }
+
 // time complexity o(n^2)
