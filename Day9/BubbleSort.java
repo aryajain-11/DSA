b0f6d1edd885ed2371e90903e0529b6d6dbafc0c
aryajain-11 <jain2004arya@gmail.com>
2025-03-23 11:24:33 +0530

    Initial Commit

--- Day9/BubbleSort.java
+++ Day9/BubbleSort.java
@@ -1,12 +1,22 @@
 public class BubbleSort {
     public static void bubbleSort(int arr[]){
+        //n-1 passes /iteration
+        //i = 0 to n-2
         for (int i = 0; i < arr.length-1; i++) {
+            boolean flag = false;//has any swapping happened
             for (int j = 0; j < arr.length-i-1; j++) {
+                /*Last i element are already at correct sorted position so need to check them
+                 */
                 if (arr[j]>arr[j+1]) {
+                    //swap
                     int temp = arr[j];
                     arr[j]= arr[j+1];
                     arr[j+1]= temp;
+                    flag = true;//some swaps happened
                 }
+            }
+            if(flag==false){ //if(!flag)  have any swaps happened
+                return;
             }
         }
 
--- Day9/InsertionSort.java
+++ Day9/InsertionSort.java
@@ -1,27 +1,23 @@
 public class InsertionSort {
-    public static void main(String[] args) {
-        int arr[] = {7,8,3,2};
-        //insertion sort
-        //unsorted k liy i and sorted part k liy j 
+    public static void insertionSort(int arr[]){
         for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i-1;
-            while (j>=0 && current<arr[j]) {
-                arr[j+1] =arr[j];
+            int j =i;
+            while (j>0 && arr[j] < arr[j-1]) {
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
                 j--;
-
                 
             }
-            //placement
-            arr[j+1] = current;
-
+            
         }
-        printArray(arr);
     }
-    public static void printArray(int arr[]){
+    public static void main(String[] args) {
+        int arr[] = {8,3,6,5,4,2};
+        insertionSort(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+ " ");
+            System.out.println(arr[i]);
         }
-        System.out.println();
     }
 }
